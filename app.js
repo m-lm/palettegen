68b86fdbc1bedb2bfac537406524cc38b97fb5d8
fechsler <31556425+fechsler@users.noreply.github.com>
2021-12-15 13:24:51 -0600

    Add tentative image display

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-
+function generate() {
+	var img = document.getElementById("img_upload").files[0];
+	console.log(img);
+	var reader = new FileReader();
+	reader.onload = function(x) {
+		var draw = document.createElement("img");
+		draw.src = x.target.result;
+		document.body.appendChild(draw);
+	}
+	reader.readAsDataURL(img);
+//	var canvas = document.getElementById("drawn");
+//	ctx = canvas.getContext("2d");
+//	ctx.drawImage(img, 100, 100);
+}
 
