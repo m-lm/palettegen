6da5dbb858ec30bab5903d4572469ef9dc85674c
fechsler <31556425+fechsler@users.noreply.github.com>
2021-12-22 16:12:21 -0600

    Tweak HTML/CSS

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 function generate() {
-	var img = document.getElementById("img_upload").files[0];
-	console.log(img);
-	var reader = new FileReader();
-	reader.onload = function(x) {
-		var draw = document.createElement("img");
-		draw.src = x.target.result;
-		document.body.appendChild(draw);
-	}
-	reader.readAsDataURL(img);
-//	var canvas = document.getElementById("drawn");
-//	ctx = canvas.getContext("2d");
-//	ctx.drawImage(img, 100, 100);
+	const img = document.getElementById("img_upload").files[0];
+	console.log(img, "type: ", typeof img);
+	console.log(img.width, "x", img.height);
 }
 
